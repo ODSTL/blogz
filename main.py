4b0bf6c11f6eb44b59c151fe15cfd71e9865be7e
ODSTL <dan@danjaboor.com>
2018-10-25 18:38:16 -0500

    FInal COde

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -28,24 +28,38 @@ def blog():
     if request.args:
         id = request.args.get('id')
         entry = Blog.query.get(id)
-        return render_template('singlepost.html', new_post=entry)
+        
+        return render_template('singlepost.html', post=entry)
 
     else:
         entry = Blog.query.all()
         return render_template('blog.html', blog=entry)
 
+#@app.route('/newpost', )
 
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
+@app.route('/newpost', methods=['GET', 'POST'])
+def new_post():
 
-    task_id = int(request.form['task-id'])
-    task = Blog.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
+    if request.method == 'POST':
 
-    return redirect('/')
+        title = request.form['title']
+        body = request.form['body']
+        fish = Blog(title, body)
+        t_error=''
+        b_error=''
+        if not title:
+            t_error = "Please Enter A Post Title"
+        if not body:
+            b_error = "Please Enter A Post Body"
+        if not t_error and not b_error:
+            post = Blog(title, body)
+            db.session.add(post)
+            db.session.commit()
+            return redirect('/blog?id=' + str(post.id))
 
+        return render_template('newpost.html', t_error=t_error, b_error=b_error, title=title, body=body)
+    else:
+        return render_template('newpost.html')
 
 if __name__ == '__main__':
     app.run()
