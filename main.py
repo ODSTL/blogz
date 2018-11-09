ba32bacc7d51cce6665119b5085b0a887feaf708
ODSTL <dan@danjaboor.com>
2018-11-09 14:48:24 -0600

    xcopde updated, so hopefully this push works

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Note: the connection string after :// contains the following info:
 # user:password@server:portNumber/databaseName
 
-from flask import Flask, request, redirect, render_template, session
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -33,22 +33,21 @@ class Blog(db.Model):
     def __init__(self, title, body, owner):
         self.title = title
         self.body = body
-        self.user = owner
+        self.owner = owner
 
 @app.before_request
 def require_login():
-    allowed_routes = ['index', 'all_blogs', 'singleUser', 'signup', 'login']
+    allowed_routes = ['/', 'newpost', 'signup', 'login']
     if request.endpoint  not in allowed_routes and "username" not in session:
         return redirect('/login')
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    users = User.query.all()
+    users = User.query.order_by(User.id.asc()).all()
     return render_template('index.html', users=users)
 
 @app.route("/newpost", methods= ['POST', 'GET'])
 def AddPost():
-    error = {"t_blank": "", "b_blank": ""}
     new_body = ""
     new_title = ""
 
@@ -56,116 +55,97 @@ def AddPost():
         title = request.form["title"]
         body = request.form["body"]
         owner = User.query.filter_by(username=session['username']).first()
-        if title == "":
-            error["t_blank"] = "Please enter a title."
-        if body == "":
-            error["b_blank"] = "Please write a post."
+        new_post = Blog(title, body, owner)
 
-        if error["t_blank"] == "" and error["b_blank"] == "":
-            new_blog = Blog(title, body, owner)
-            db.session.add(new_blog)
+        if new_post.title == "":
+            flash("Please enter a title.", "error")
+        if new_post.body == "":
+            flash("Please write a post.", "error")
+
+        if new_post.title != "" and new_post.body != "":
+            db.session.add(new_post)
             db.session.commit()
-            return redirect("/all_blogs?id="+str(new_blog.id))
+            return redirect("/all_blogs?id="+str(new_post.id))
 
-        return render_template('newpost.html', title= "Please add a post.",
-            body= body, owner= owner, t_blank= error["t_blank"],
-             b_blank= error["b_blank"])
-
-    else:
-        return render_template('newpost.html')
+        else:
+            return render_template('newpost.html')
+    return render_template('newpost.html')
 
 @app.route("/all_blogs", methods=['GET'])
 def AllBlogs():
-    if request.args:
-        id = request.args.get ('id')
-        post = Blog.query.get(id)
-        return render_template('singlepost.html', entry=post)
-    else:
-        post = blog.query.all()
-        return render_template("all_blogs.html", blog=post)
+    if "user" in request.args:
+        user_id = request.args.get ('user')
+        post = Blog.query.filter_by(id=id).first()
+        return render_template('singlepost.html', title="Your Posts", post=post)
+
+    posts = Blog.query.order_by(Blog.id.asc())
+    return render_template('all_blogs.html', title="List", posts=posts)
 
 @app.route("/singleUser", methods=['POST', 'GET'])
 def SingleUser():
 
     user = request.args.get('user')
-    blogs = Blog.query.filter_by(owner_id= user).all()
+    blogs = Blog.query.filter_by(owner_id=user).all()
 
     return render_template('singleUser.html', blogs= blogs)
 
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user and existing_user.password == password:
+            session['username'] = username
+            flash("You are logged in.")
+            return redirect('/all_blogs')
+        if existing_user and existing_user.password != password:
+            flash("Password Incorrect.", "error")
+
+    return render_template("login.html")
+
+
 @app.route("/signup", methods=['POST', 'GET'])
 def signup():
-    error = {"u_error": "", "p_error": "", "v_error": ""}
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-        username_value = ""
-        password_value = ""
-        verify_value = ""
         existing_user = User.query.filter_by(username=username).first()
 
-        if existing_user:
-            username_value = "Please choose a different username."
-        elif username == "":
-            username_value = "Username cannot be blank."
-        elif " " in username:
-            username_value = "Username cannot contain spaces."
+        if username == "":
+            flash("Username cannot be blank.", "error")
+        if len(username) < 3:
+            flash("Username must contain more than 3 characters.", "error")
+        if " " in username:
+            flash("Username cannot contain spaces.", "error")
 
         if password == "":
-           password_value = "Password cannot be blank."
-        elif len(password) < 5:
-           password_value = "Password must contain more than 5 characters."
-        elif " " in password:
-           password_value = "Password cannot contain spaces."
+            flash("Password cannot be blank.", "error")
+        if len(password) < 3:
+            flash("Password must contain more than 3 characters.", "error")
+        if " " in password:
+            flash("Password cannot contain spaces.", "error")
 
         if verify == "":
-           verify_value = "Please complete all fields."
-        elif verify != password:
-           verify_value = "Request could not be verified. Please try again."
-        else:
-           if password != verify:
-               error["v_error"] = "Passwords must match."
+            flash("Please complete all fields.", "error")
+        if verify != password:
+            flash("Request could not be verified. Please try again.", "error")
 
-        if not existing_user and not username_value and not password_value and not verify_value:
-           user = User(username, password)
-           db.session.add(user)
-           db.session.commit()
-           session['username'] = username
-           return redirect('/newpost')
-
-        else:
-           return render_template("signup.html",
-            username_value=username_value, password_value=password_value,
-            verify_value=verify_value, username=username)
+            if not existing_user and not username and not password and not verify:
+                user = User(username, password)
+                db.session.add(user)
+                db.session.commit()
+                session['username'] = username
+                return redirect('/newpost')
+            else:
+                return render_template('signup.html')
 
     return render_template("signup.html")
 
 
-@app.route("/login", methods=['POST', 'GET'])
-def login():
-    error = {"u_error": "", "p_error": ""}
-    username = ""
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        u_error = ''
-        p_error = ''
-        existing_user = User.query.filter_by(username=username).first()
-
-        if not username:
-            u_error = "Invalid Username."
-        if not password:
-            p_error = "Invalid Password."
-        if user and user.password == password:
-            session['username'] = username
-            return redirect('/all_blogs')
-        else:
-            p_error = "Invalid Password."
-
-        return render_template("login.html", u_error= u_error,
-            p_error= p_error)
-    return render_template("login.html")
 
 
 @app.route("/logout")
